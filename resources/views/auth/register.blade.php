f2b457e67376aa33bada09a925208648a1d83dbf
Nestorpq <128406378+NestorPQ@users.noreply.github.com>
2023-05-26 22:07:19 -0500

    primer commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <form action="/register" method="POST">
+        @csrf
+        <input type="text" name="username" placeholder="Usuario" required>
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Contraseña" required>
+        <input type="password" name="password_confirmation" placeholder="Contraseña nuevamente" required>
+        <input type="submit" value="Registrar">
+    </form>
+</body>
+</html>
